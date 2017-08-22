f53acca347fcab67d4477bd7a447d9a5f37fbfe3
namnami121 <jnkim@popcornsar.com>
2017-08-22 11:22:38 +0900

    iphone layout  깨짐 수정 및 jp page img변경

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(80, function(){
-    console.log("Express server has started on port 80")
+    console.log("Express server has started on port 80");
 });
 app.use(express.static('public'));
 app.use(express.static('vendor'));
--- router/main.js
+++ router/main.js
@@ -1,12 +1,19 @@
+var useragent= require('useragent');
+useragent(true);
+
 module.exports = function(app)
 {
      app.get('/',function(req,res){
-        res.render('main_kr.html')
+        var agent = useragent.parse(req.headers['user-agent']);
+        console.log("device version"+agent.toString());
+        console.log("device version"+agent);
+        res.render('main_kr.html');
+
      });
     app.get('/main_en',function(req,res){
-        res.render('main_en.html')
+        res.render('main_en.html');
      });
     app.get('/main_jp',function(req,res){
-        res.render('main_jp.html')
+        res.render('main_jp.html');
      });
 }
